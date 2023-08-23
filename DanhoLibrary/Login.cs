e6849323fc847e8d2053fb1ac5fcf1cdc639e78b
DanielSimonsen90 <danielsimonsen90@gmail.com>
2023-08-23 21:29:48 +0200

    Basic login model

---
+++ DanhoLibrary/Login.cs
@@ -0,0 +1,13 @@
+﻿namespace DanhoLibrary;
+
+public class Login
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public Login(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+}
